89ed7c3bb9510aa22a8ae67a2c64371e1503629d
Osni Antônio Küchler <osni.antonio@gmail.com>
2024-02-26 20:26:48 -0300

    Fazendo a busca do CEP via api Brasil API e pela ViaCEP

--- main.go
+++ main.go
@@ -57,41 +57,38 @@ func BuscarCEP(url string, ch chan<- *Endereco) error {
 }
 
 func main() {
-	var QtdeTentativas int = 10
 
 	cep := "01153000"
 
 	ch := make(chan *Endereco)
 
-	for i := 0; i < QtdeTentativas; i++ {
-		urlBrasilAPI := fmt.Sprintf("https://brasilapi.com.br/api/cep/v1/%s", cep)
-		go BuscarCEP(urlBrasilAPI, ch)
+	urlBrasilAPI := fmt.Sprintf("https://brasilapi.com.br/api/cep/v1/%s", cep)
+	go BuscarCEP(urlBrasilAPI, ch)
 
-		urlViaCepAPI := fmt.Sprintf("http://viacep.com.br/ws/%s/json/", cep)
-		go BuscarCEP(urlViaCepAPI, ch)
+	urlViaCepAPI := fmt.Sprintf("http://viacep.com.br/ws/%s/json/", cep)
+	go BuscarCEP(urlViaCepAPI, ch)
 
-		var api1, api2 *Endereco
+	var api1, api2 *Endereco
 
-		select {
-		case api1 = <-ch:
-			fmt.Println("Resposta da API 1 (brasilapi.com.br):")
-			if api1 != nil {
-				jsonData, _ := json.Marshal(api1)
-				fmt.Println(string(jsonData))
-			} else {
-				fmt.Println("Erro ao buscar endereço na API 1")
-			}
-		case api2 = <-ch:
-			fmt.Println("Resposta da API 2 (viacep.com.br):")
-			if api2 != nil {
-				jsonData, _ := json.Marshal(api2)
-				fmt.Println(string(jsonData))
-			} else {
-				fmt.Println("Erro ao buscar endereço na API 2")
-			}
-		case <-time.After(time.Second):
-			fmt.Println("Timeout: Nenhuma resposta recebida em 1 segundo.")
+	select {
+	case api1 = <-ch:
+		fmt.Println("Resposta da API 1 (brasilapi.com.br):")
+		if api1 != nil {
+			jsonData, _ := json.Marshal(api1)
+			fmt.Println(string(jsonData))
+		} else {
+			fmt.Println("Erro ao buscar endereço na API 1")
 		}
+	case api2 = <-ch:
+		fmt.Println("Resposta da API 2 (viacep.com.br):")
+		if api2 != nil {
+			jsonData, _ := json.Marshal(api2)
+			fmt.Println(string(jsonData))
+		} else {
+			fmt.Println("Erro ao buscar endereço na API 2")
+		}
+	case <-time.After(time.Second):
+		fmt.Println("Timeout: Nenhuma resposta recebida em 1 segundo.")
 	}
 
 }
