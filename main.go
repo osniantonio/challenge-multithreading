f9fbcec8e6dfaf1f0be8980e7227f92281bad9f9
Osni Antônio Küchler <osni.antonio@gmail.com>
2024-02-26 20:24:55 -0300

    Fazendo a busca do CEP via api Brasil API e pela ViaCEP

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type Endereco interface{}
+
+type EnderecoBrasilAPI struct {
+	Cep          string `json:"cep"`
+	City         string `json:"city"`
+	Neighborhood string `json:"neighborhood"`
+	Service      string `json:"service"`
+	State        string `json:"state"`
+	Street       string `json:"street"`
+}
+
+type EnderecoViaCEP struct {
+	Bairro      string `json:"bairro"`
+	Cep         string `json:"cep"`
+	Complemento string `json:"complemento"`
+	DDD         string `json:"ddd"`
+	GIA         string `json:"gia"`
+	IBGE        string `json:"ibge"`
+	Localidade  string `json:"localidade"`
+	Logradouro  string `json:"logradouro"`
+	Siafi       string `json:"siafi"`
+	UF          string `json:"uf"`
+}
+
+func BuscarCEP(url string, ch chan<- *Endereco) error {
+	startTime := time.Now()
+
+	client := http.Client{Timeout: time.Second}
+	resp, err := client.Get(url)
+	if err != nil {
+		ch <- nil
+		return err
+	}
+	defer resp.Body.Close()
+
+	var endereco Endereco
+	if err := json.NewDecoder(resp.Body).Decode(&endereco); err != nil {
+		ch <- nil
+		return err
+	}
+
+	elapsedTime := time.Since(startTime)
+	fmt.Printf("Tempo de execução (%s): %v\n", url, elapsedTime)
+
+	ch <- &endereco
+
+	return nil
+}
+
+func main() {
+	var QtdeTentativas int = 10
+
+	cep := "01153000"
+
+	ch := make(chan *Endereco)
+
+	for i := 0; i < QtdeTentativas; i++ {
+		urlBrasilAPI := fmt.Sprintf("https://brasilapi.com.br/api/cep/v1/%s", cep)
+		go BuscarCEP(urlBrasilAPI, ch)
+
+		urlViaCepAPI := fmt.Sprintf("http://viacep.com.br/ws/%s/json/", cep)
+		go BuscarCEP(urlViaCepAPI, ch)
+
+		var api1, api2 *Endereco
+
+		select {
+		case api1 = <-ch:
+			fmt.Println("Resposta da API 1 (brasilapi.com.br):")
+			if api1 != nil {
+				jsonData, _ := json.Marshal(api1)
+				fmt.Println(string(jsonData))
+			} else {
+				fmt.Println("Erro ao buscar endereço na API 1")
+			}
+		case api2 = <-ch:
+			fmt.Println("Resposta da API 2 (viacep.com.br):")
+			if api2 != nil {
+				jsonData, _ := json.Marshal(api2)
+				fmt.Println(string(jsonData))
+			} else {
+				fmt.Println("Erro ao buscar endereço na API 2")
+			}
+		case <-time.After(time.Second):
+			fmt.Println("Timeout: Nenhuma resposta recebida em 1 segundo.")
+		}
+	}
+
+}
