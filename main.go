757867bf0ba97d91df4f6e14ea602965780485e3
Osni Antônio Küchler <osni.antonio@gmail.com>
2024-02-27 21:45:11 -0300

    Ajustado e será feita a entrega com essa versão

--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 	"time"
 )
 
-type Endereco interface{}
-
 type EnderecoBrasilAPI struct {
 	Cep          string `json:"cep"`
 	City         string `json:"city"`
@@ -31,9 +29,8 @@ type EnderecoViaCEP struct {
 	UF          string `json:"uf"`
 }
 
-func BuscarCEP(url string, ch chan<- *Endereco) error {
-	startTime := time.Now()
-
+// receive-only: diz que esse canal somente recebe dados (seta do lado direito)
+func BuscarEnderecoViaBrasilAPI(url string, ch chan<- *EnderecoBrasilAPI) error {
 	client := http.Client{Timeout: time.Second}
 	resp, err := client.Get(url)
 	if err != nil {
@@ -42,53 +39,85 @@ func BuscarCEP(url string, ch chan<- *Endereco) error {
 	}
 	defer resp.Body.Close()
 
-	var endereco Endereco
+	var endereco EnderecoBrasilAPI
 	if err := json.NewDecoder(resp.Body).Decode(&endereco); err != nil {
 		ch <- nil
 		return err
 	}
 
-	elapsedTime := time.Since(startTime)
-	fmt.Printf("Tempo de execução (%s): %v\n", url, elapsedTime)
+	ch <- &endereco
+	return nil
+}
+
+// receive-only: diz que esse canal somente recebe dados (seta do lado direito)
+func BuscarEnderecoViaCepAPI(url string, ch chan<- *EnderecoViaCEP) error {
+	client := http.Client{Timeout: time.Second}
+	resp, err := client.Get(url)
+	if err != nil {
+		ch <- nil
+		return err
+	}
+	defer resp.Body.Close()
+
+	var endereco EnderecoViaCEP
+	if err := json.NewDecoder(resp.Body).Decode(&endereco); err != nil {
+		ch <- nil
+		return err
+	}
 
 	ch <- &endereco
-
 	return nil
 }
 
 func main() {
 
+	// CEP para consulta
 	cep := "01153000"
 
-	ch := make(chan *Endereco)
+	canalBrasil := make(chan *EnderecoBrasilAPI)
+	canalViaCEP := make(chan *EnderecoViaCEP)
 
+	// As duas requisições serão feitas simultaneamente para as seguintes APIs:
 	urlBrasilAPI := fmt.Sprintf("https://brasilapi.com.br/api/cep/v1/%s", cep)
-	go BuscarCEP(urlBrasilAPI, ch)
-
 	urlViaCepAPI := fmt.Sprintf("http://viacep.com.br/ws/%s/json/", cep)
-	go BuscarCEP(urlViaCepAPI, ch)
+	go BuscarEnderecoViaCepAPI(urlViaCepAPI, canalViaCEP)
+	go BuscarEnderecoViaBrasilAPI(urlBrasilAPI, canalBrasil)
 
-	var api1, api2 *Endereco
+	var brasilAPI *EnderecoBrasilAPI
+	var viaCepAPI *EnderecoViaCEP
 
+	var vencedora string
+	var resultado interface{}
+
+	// selecionando a api vencedora
 	select {
-	case api1 = <-ch:
-		fmt.Println("Resposta da API 1 (brasilapi.com.br):")
-		if api1 != nil {
-			jsonData, _ := json.Marshal(api1)
-			fmt.Println(string(jsonData))
+	case brasilAPI = <-canalBrasil:
+		if brasilAPI != nil {
+			vencedora = "BrasilAPI"
+			resultado = brasilAPI
 		} else {
-			fmt.Println("Erro ao buscar endereço na API 1")
+			fmt.Println("Falha ao tentar buscar o endereço via BrasilAPI")
 		}
-	case api2 = <-ch:
-		fmt.Println("Resposta da API 2 (viacep.com.br):")
-		if api2 != nil {
-			jsonData, _ := json.Marshal(api2)
-			fmt.Println(string(jsonData))
+	case viaCepAPI = <-canalViaCEP:
+		if viaCepAPI != nil {
+			vencedora = "ViaCEP"
+			resultado = viaCepAPI
 		} else {
-			fmt.Println("Erro ao buscar endereço na API 2")
+			fmt.Println("Falha ao tentar buscar o endereço via ViaCepAPI")
 		}
 	case <-time.After(time.Second):
-		fmt.Println("Timeout: Nenhuma resposta recebida em 1 segundo.")
+		fmt.Println("Timeout: nenhuma resposta foi recebida em 1 segundo.")
 	}
 
+	// mostrar o resultado da vencedora
+	if vencedora != "" {
+		fmt.Printf("API vencedora: %s\n", vencedora)
+		fmt.Println("Resultado:")
+		jsonData, err := json.Marshal(resultado)
+		if err != nil {
+			fmt.Println("Falha ao tentar serializar o resultado da busca:", err)
+		} else {
+			fmt.Println(string(jsonData))
+		}
+	}
 }
